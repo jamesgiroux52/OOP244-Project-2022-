c8d26648a36e4c658b79a5a3c75f0bcba3635c37
jru5252 <jru5252@gmail.com>
2022-12-03 17:00:15 -0500

    MS5

--- ms5/main.cpp
+++ ms5/main.cpp
@@ -12,12 +12,12 @@ void runParking(long numSpots = 10);
 void Datafile(bool restore = false); // Change to true if you want to revert to original csv file
 
 // accepts an argument for number of parking spots
-// eg. './name_of_exe 50' will start the program with 50 spots *** between 1-100 ***
-// if no argument is passed or it is not between 1 -100 then the deafault 10 spots is used
+// eg. './name_of_exe 50' will start the program with 50 spots *** between 10-100 ***
+// if no argument is passed or it is not between 10 -100 then the deafault 10 spots is used
 int main(int argc, char* argv[]) {
     if (argc > 1) {
         long numberOfSpots = strtol(argv[1], nullptr, 10);
-        if (numberOfSpots > 0 && numberOfSpots <= 100)
+        if (numberOfSpots > 10 && numberOfSpots <= 100)
             runParking(numberOfSpots);
         else
             runParking();
